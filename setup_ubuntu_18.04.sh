e894ed7720ed642ceaaeb6d2fd45be612291eaa2
iovsiann <iovsiann@users.noreply.github.com>
2019-11-13 02:23:03 -0800

    Create setup_ubuntu_18.04.sh

---
+++ setup_ubuntu_18.04.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git curl chromium-browser openssh-server -y
+sudo apt-get remove unattended-upgrades firefox -y
+sudo ntpdate ntp.ubuntu.com
+sudo iw reg set US
+sed -i 's/HISTSIZE=[0-9]\+/HISTSIZE=/' .bashrc
+sed -i 's/HISTFILESIZE=[0-9]\+/HISTFILESIZE=/' .bashrc
