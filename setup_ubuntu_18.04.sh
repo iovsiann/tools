2dc931c0b96d99a5ee67798c18357c8b1840a9f4
iovsiann <iovsiann@users.noreply.github.com>
2019-11-29 01:31:10 -0800

    Cache git credentials

--- setup_ubuntu_18.04.sh
+++ setup_ubuntu_18.04.sh
@@ -8,3 +8,5 @@ sudo ntpdate ntp.ubuntu.com
 sudo iw reg set US
 sed -i 's/HISTSIZE=[0-9]\+/HISTSIZE=/' .bashrc
 sed -i 's/HISTFILESIZE=[0-9]\+/HISTFILESIZE=/' .bashrc
+git config credential.helper store
+git config --global credential.helper "cache --timeout 7200"
