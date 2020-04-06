3aed7c9a7c7e56df861bcd6cb56264758d84a628
iovsiann <iovsiann@users.noreply.github.com>
2020-04-06 12:47:44 -0700

    Create teleop_key.sh

---
+++ launch/turtlebot3/teleop_key.sh
@@ -0,0 +1,10 @@
+gnome-terminal -e "roscore"
+sleep 2
+gnome-terminal -e "roslaunch turtlebot3_bringup turtlebot3_remote.launch"
+sleep 2
+roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch
+sleep 2
+gnome-terminal -e "rosrun rviz rviz -d `rospack find turtlebot3_description`/rviz/model.rviz"
+sleep 2
+gnome-terminal -e "rqt"
+sleep 2
