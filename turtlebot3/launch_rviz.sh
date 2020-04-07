ddbe07724ce1f574b3553c55df17e5b3859c25cf
iovsiann <iovsiann@users.noreply.github.com>
2020-04-06 19:12:23 -0700

    Update launch_rviz.sh

--- turtlebot3/launch_rviz.sh
+++ turtlebot3/launch_rviz.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Launch rviz
 
-roslaunch turtlebot3_bringup turtlebot3_remote.launch
+roslaunch turtlebot3_bringup turtlebot3_remote.launch &
+sleep 2
 rosrun rviz rviz -d `rospack find turtlebot3_description`/rviz/model.rviz
