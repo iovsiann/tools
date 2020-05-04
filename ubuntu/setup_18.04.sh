57872cb85701b72139f0237f86f85e198a8b7a98
iovsiann <iovsiann@users.noreply.github.com>
2020-05-04 02:41:16 -0700

    Update setup_18.04.sh

--- ubuntu/setup_18.04.sh
+++ ubuntu/setup_18.04.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get remove unattended-upgrades firefox -y
 sudo apt-get install git curl chromium-browser openssh-server terminator net-tools -y
-git config credential.helper cache
+git config --global credential.helper cache
 sudo ntpdate ntp.ubuntu.com
 sudo iw reg set US
 sed -i 's/HISTSIZE=[0-9]\+/HISTSIZE=/' .bashrc
