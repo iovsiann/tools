720ad9e305d6ed6a33f56df96b939bdcc081fc64
iovsiann <iovsiann@users.noreply.github.com>
2020-06-16 22:12:47 -0700

    Create install_turtlebot3.sh

---
+++ rpi/install_turtlebot3.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+catkin_ws
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin_make
+echo "source $HOME/catkin_ws/devel/setup.bash" >> ~/.bashrc
+
+cd ~/catkin_ws/src
+
+git clone https://github.com/ROBOTIS-GIT/hls_lfcd_lds_driver.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone https://github.com/UbiquityRobotics/raspicam_node.git
+cd ~/catkin_ws/src/turtlebot3
+rm -r turtlebot3_description/ turtlebot3_teleop/ turtlebot3_navigation/ turtlebot3_slam/ turtlebot3_example/
+cd ~/catkin_ws && catkin_make
+rosrun turtlebot3_bringup create_udev_rules
+ipconfig
