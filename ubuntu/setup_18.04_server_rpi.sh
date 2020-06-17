66ca2a3f8ed2481eb1d874d95d323a0aa529778b
iovsiann <iovsiann@users.noreply.github.com>
2020-06-16 20:07:59 -0700

    Create setup_18.04_server_rpi.sh

---
+++ ubuntu/setup_18.04_server_rpi.sh
@@ -0,0 +1,16 @@
+  
+#!/bin/sh
+
+sudo systemctl mask apt-daily.service apt-daily-upgrade.service
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git iw -y
+git conf credential.helper store
+git config --global credential.helper 'cache --timeout=7200'
+sudo ntpdate ntp.ubuntu.com
+sed -i 's/HISTSIZE=[0-9]\+/HISTSIZE=/' .bashrc
+sed -i 's/HISTFILESIZE=[0-9]\+/HISTFILESIZE=/' .bashrc
+sudo iw reg set US
+
+# https://linuxconfig.org/ubuntu-20-04-connect-to-wifi-from-command-line
+# Change WiFi SSID to 5G
