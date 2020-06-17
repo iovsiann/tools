f671cfd732287a6d564f57343c66d41d51f60cfe
iovsiann <iovsiann@users.noreply.github.com>
2020-06-16 20:09:07 -0700

    Update setup_18.04_server_rpi.sh

--- ubuntu/setup_18.04_server_rpi.sh
+++ ubuntu/setup_18.04_server_rpi.sh
@@ -4,7 +4,7 @@
 sudo systemctl mask apt-daily.service apt-daily-upgrade.service
 sudo apt update
 sudo apt upgrade -y
-sudo apt install git iw -y
+sudo apt install git iw raspi-config -y
 git conf credential.helper store
 git config --global credential.helper 'cache --timeout=7200'
 sudo ntpdate ntp.ubuntu.com
