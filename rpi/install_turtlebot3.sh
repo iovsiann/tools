0db2f729da937b603c782e683b16f495029e2f75
iovsiann <iovsiann@users.noreply.github.com>
2020-06-17 00:45:54 -0700

    Update install_turtlebot3.sh

--- rpi/install_turtlebot3.sh
+++ rpi/install_turtlebot3.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
+cd ~/ros_ws
+rosinstall_generator common_msgs common_msgs rosserial image_common image_transport_plugins diagnostics nodelet_core --rosdistro melodic --deps --wet-only --tar > melodic-extra-wet.rosinstall
+wstool merge -t src melodic-extra-wet.rosinstall
+wstool update -j8 -t src
+rosdep install --from-paths src --ignore-src --rosdistro melodic -y
+sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/melodic
+
 catkin_ws
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/
@@ -7,7 +14,6 @@ catkin_make
 echo "source $HOME/catkin_ws/devel/setup.bash" >> ~/.bashrc
 
 cd ~/catkin_ws/src
-
 git clone https://github.com/ROBOTIS-GIT/hls_lfcd_lds_driver.git
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
 git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
@@ -18,4 +24,3 @@ cd ~/catkin_ws && catkin_make
 rosrun turtlebot3_bringup create_udev_rules
 sudo apt install ntpdate
 sudo ntpdate ntp.ubuntu.com
-ifconfig
