b8fcbb6bbdb6a53358c19e3c6a89e4192317019f
iovsiann <iovsiann@users.noreply.github.com>
2020-06-21 07:31:35 -0700

    Update install_python_libs.sh

--- rpi/install_python_libs.sh
+++ rpi/install_python_libs.sh
@@ -3,7 +3,8 @@
 sudo apt install pypy3-dev python3-pyaudio libportaudio19-dev -y
 cd &&  wget https://bootstrap.pypa.io/get-pip.py
 pypy3 get-pip.py
-python3 get-pip.py && rm get-pip.py
+python3 get-pip.py
+python get-pip.py && rm get-pip.py
 
 git clone https://github.com/iovsiann/pyaudio
 cd pyaudio && pypy3 -m pip install .
