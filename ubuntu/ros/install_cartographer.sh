f8a09f5bf8c99eb848d4193d5fb78779c40adcc7
iovsiann <iovsiann@users.noreply.github.com>
2021-05-31 09:10:32 -0700

    rosdep init error fix

--- ubuntu/ros/install_cartographer.sh
+++ ubuntu/ros/install_cartographer.sh
@@ -7,14 +7,16 @@ sudo apt-get install -y python-wstool python-rosdep ninja-build stow
 
 if [ ! -d "~/catkin_ws" ] 
 then
-    mkdir ~/catkin_ws
+    mkdir -p ~/catkin_ws/src
+    cd ~/catkin_ws
+    catkin_make
+    sudo rosdep init
 fi
 
 cd ~/catkin_ws
 wstool init src
 wstool merge -t src https://raw.githubusercontent.com/cartographer-project/cartographer_ros/master/cartographer_ros.rosinstall
 wstool update -t src
-sudo rosdep init
 rosdep update
 rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
 source ~/catkin_ws/src/cartographer/scripts/install_abseil.sh
