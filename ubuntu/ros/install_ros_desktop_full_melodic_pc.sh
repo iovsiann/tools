43442dcfb9a6a94b9d75b882e5c987afc0857795
iovsiann <iovsiann@users.noreply.github.com>
2021-06-03 11:45:51 -0700

    Install Cartographer

--- ubuntu/ros/install_ros_desktop_full_melodic_pc.sh
+++ ubuntu/ros/install_ros_desktop_full_melodic_pc.sh
@@ -6,7 +6,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 sudo apt update
-sudo apt install ros-melodic-desktop-full ros-melodic-navigation ros-melodic-gmapping -y
+sudo apt install ros-melodic-desktop-full -y
 
 sudo rosdep init
 rosdep update
@@ -15,6 +15,8 @@ echo 'source /opt/ros/melodic/setup.bash' >> ~/.bashrc
 source /opt/ros/melodic/setup.bash
 sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
 sudo apt install ros-melodic-joint-state-publisher-gui -y
+sudo apt install ros-melodic-navigation ros-melodic-gmapping -y
+sudo apt install ros-melodic-cartographer ros-melodic-cartographer-rviz -y
 
 echo export ROS_MASTER_URI=http://$(hostname -I | awk '{print $1}'):11311 >> ~/.bashrc
 echo export ROS_HOSTNAME=$(hostname -I | awk '{print $1}') >> ~/.bashrc
