9b3ac4064dc213b05f3e029495b74e0dd5e95e62
iovsiann <iovsiann@users.noreply.github.com>
2021-06-03 09:01:53 -0700

    Install navigation, gmapping

--- ubuntu/ros/install_ros_desktop_full_melodic_pc.sh
+++ ubuntu/ros/install_ros_desktop_full_melodic_pc.sh
@@ -6,7 +6,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 sudo apt update
-sudo apt install ros-melodic-desktop-full -y
+sudo apt install ros-melodic-desktop-full ros-melodic-navigation ros-melodic-gmapping -y
 
 sudo rosdep init
 rosdep update
