bdf8510dcb1a3f3334df5b3cc29c5173435cc1d5
iovsiann <iovsiann@users.noreply.github.com>
2021-07-13 20:45:29 -0700

    Install portaudio19-dev libffi-dev -y

--- rpi/clean_install_setup.sh
+++ rpi/clean_install_setup.sh
@@ -9,4 +9,4 @@ sed -i 's/HISTFILESIZE=[0-9]\+/HISTFILESIZE=/' .bashrc
 # rm ./VNC-Server-6.6.0-Linux-x64.deb
 # sudo systemctl start vncserver-x11-serviced
 # sudo systemctl enable vncserver-x11-serviced
-sudo apt install virtualenv mpg321 libatlas-base-dev python-pyaudio -y
+sudo apt install virtualenv mpg321 libatlas-base-dev python-pyaudio portaudio19-dev libffi-dev -y
