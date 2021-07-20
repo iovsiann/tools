5826883ec4a49f9c3b889e384d8529eaa2ea7702
iovsiann <iovsiann@users.noreply.github.com>
2021-07-19 20:36:43 -0700

    Update clean_install_setup.sh

--- rpi/clean_install_setup.sh
+++ rpi/clean_install_setup.sh
@@ -11,8 +11,9 @@ sed -i 's/HISTFILESIZE=[0-9]\+/HISTFILESIZE=/' .bashrc
 # sudo systemctl start vncserver-x11-serviced
 # sudo systemctl enable vncserver-x11-serviced
 sudo apt install virtualenv mpg321 libatlas-base-dev python-pyaudio portaudio19-dev libffi-dev python3-pyaudio -y
-sudo apt install libatlas3-base libopenblas-base -y
+sudo apt install libatlas3-base libopenblas-base libfftw3-3 -y
 # python3-numpy
-sudo systemctl disable avahi-daemon  # Bonjour local discovery
+# sudo apt install avahi-daemon
+sudo systemctl disable avahi-daemon  # Bonjour local discovery http://raspberrypi.local
 sudo systemctl disable triggerhappy  # GUI shortcuts
 systemctl disable bluetooth
