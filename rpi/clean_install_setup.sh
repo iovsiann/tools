f5eaec236ff295e7282c9ae2a7b197bfb67e70fe
iovsiann <iovsiann@users.noreply.github.com>
2021-07-20 13:27:58 -0700

    Update clean_install_setup.sh

--- rpi/clean_install_setup.sh
+++ rpi/clean_install_setup.sh
@@ -10,8 +10,8 @@ sed -i 's/HISTFILESIZE=[0-9]\+/HISTFILESIZE=/' .bashrc
 # rm ./VNC-Server-6.6.0-Linux-x64.deb
 # sudo systemctl start vncserver-x11-serviced
 # sudo systemctl enable vncserver-x11-serviced
-sudo -H pip2 install --upgrade pip
-sudo -H pip3 install --upgrade pip
+pip2 install --upgrade pip
+pip3 install --upgrade pip
 sudo apt install virtualenv mpg321 libatlas-base-dev python-pyaudio portaudio19-dev libffi-dev python3-pyaudio -y
 sudo apt install libatlas3-base libatlas-base-dev libblas3 libblas-dev libopenblas-base libopenblas-dev -y
 sudo apt install libfftw3-3 libfftw3-dev libfftw3-mpi3 libfftw3-mpi-dev python3-numpy python-numpy -y
