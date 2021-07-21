3f161bec123dbfc01176af0d167fa97d886b781d
iovsiann <iovsiann@users.noreply.github.com>
2021-07-20 21:09:57 -0700

    Update clean_install_setup.sh

--- rpi/clean_install_setup.sh
+++ rpi/clean_install_setup.sh
@@ -12,10 +12,15 @@ sed -i 's/HISTFILESIZE=[0-9]\+/HISTFILESIZE=/' .bashrc
 # sudo systemctl enable vncserver-x11-serviced
 pip2 install --upgrade pip
 pip3 install --upgrade pip
-sudo apt install virtualenv mpg321 libatlas-base-dev python-pyaudio portaudio19-dev libffi-dev python3-pyaudio -y
-sudo apt install libblas3 libblas-dev libopenblas-dev -y
-sudo apt install libfftw3-3 libfftw3-dev libfftw3-mpi3 libfftw3-mpi-dev python3-numpy python-numpy -y
-sudo apt install liblapack-dev liblapacke-dev liblapack3 liblapack-pic liblapacke -y
+
+sudo apt install libatlas-base-dev libopenblas-dev -y
+sudo apt install virtualenv mpg321 python-pyaudio portaudio19-dev libffi-dev python3-pyaudio -y
+# sudo apt install libblas3 libblas-dev -y
+# sudo apt install libfftw3-3 libfftw3-dev libfftw3-mpi3 libfftw3-mpi-dev python3-numpy python-numpy -y
+# sudo apt install liblapack-dev liblapacke-dev liblapack3 liblapack-pic liblapacke -y
+
+pip3 install numpy
+
 # sudo apt install avahi-daemon
 sudo systemctl disable avahi-daemon  # Bonjour local discovery http://raspberrypi.local
 sudo systemctl disable triggerhappy  # GUI shortcuts
