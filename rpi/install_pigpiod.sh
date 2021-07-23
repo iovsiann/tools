1dc5bc81a264a80bcdb97146875f40368a7ffa7c
iovsiann <iovsiann@users.noreply.github.com>
2021-07-23 16:33:59 -0700

    Moved to private repo

--- rpi/install_pigpiod.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo cp ./pigpiod.service /etc/systemd/system/
-sudo systemctl start pigpiod
-sudo systemctl enable pigpiod
-# sudo systemctl status pigpiod
-# sudo systemctl stop pigpiod
