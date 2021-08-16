12f2770875a53fee5352c977bbdd4b1e4d0dbcd6
iovsiann <iovsiann@users.noreply.github.com>
2021-08-15 18:30:21 -0700

    Correct ROS1 key

--- rpi/install_ros_core_melodic_rpi.sh
+++ rpi/install_ros_core_melodic_rpi.sh
@@ -3,7 +3,9 @@
 # on Raspberry Pi 4b
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt install curl -y
+# sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake
 sudo rosdep init
