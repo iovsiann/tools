74ec6956b9a38917b78c74779b039313a1348d12
iovsiann <iovsiann@users.noreply.github.com>
2021-08-19 23:42:08 -0700

    Update install_ros_desktop_full_melodic_pc.sh

--- ubuntu/ros/install_ros_desktop_full_melodic_pc.sh
+++ ubuntu/ros/install_ros_desktop_full_melodic_pc.sh
@@ -2,6 +2,7 @@
 # Install ROS Melodic desktop_full (with GUI) on Ubuntu 18.04
 # http://wiki.ros.org/melodic/Installation/Ubuntu
 
+# sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt install curl -y
 # sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
